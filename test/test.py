a0e911859d8cd19a5934b40116a4983e40ff4989
batinduz <batinduz@gmail.com>
2017-05-27 13:26:46 +0300

    deburjin alignment

--- test/test.py
+++ test/test.py
@@ -1,7 +1,18 @@
-from  biyoenformatik.mkt.core.direction.mkt_direction import Direction
+def de_bruijn(k, n):
+    a = [0] * k * n
+    sequence = []
+    def db(t, p):
+        if t > n:
+            if n % p == 0:
+                for j in range(1, p + 1):
+                    sequence.append(a[j])
+        else:
+            a[t] = a[t - p]
+            db(t + 1, p)
+            for j in range(a[t - p] + 1, k):
+                a[t] = j
+                db(t + 1, t)
+    db(1, 1)
+    return sequence
 
-direction = Direction(5)
-direction.complementary()
-
-print (direction.start)
-print (direction)
+print de_bruijn(2,9)
